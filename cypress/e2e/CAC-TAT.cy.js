6f9fda2a3e73b5fb2ffad96ded0941787b68f257
beatrizcunharo <beatrizcunharo@gmail.com>
2025-08-05 00:05:13 -0300

    feat: usando alias e seleção de arquivos

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -96,4 +96,24 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.fillMandatoryFieldsAndSubmit()
     cy.get('.error').should('be.visible')
   })
+
+  it('seleciona um arquivo da pasta fixtures', () => {
+    cy.get('#file-upload').selectFile('cypress/fixtures/example.json').should(input => {
+      expect(input[0].files[0].name).to.equal('example.json')
+    })
+  })
+
+  it('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('#file-upload').selectFile('cypress/fixtures/example.json', { action: 'drag-drop' }).should(input => {
+      expect(input[0].files[0].name).to.equal('example.json')
+    })
+  })
+
+  it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json').as('sampleFile')
+    cy.get('#file-upload').selectFile('@sampleFile').should(input => {
+      expect(input[0].files[0].name).to.equal('example.json')
+    })
+  })
+
 })
